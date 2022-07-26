38f767e911e3e28ba9d1a993ae567766feac8d02
Alexandre Eugene <you@example.com>
2022-07-26 13:47:02 +0200

    inial bdd

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
 
-exports.clientPromise = mongoose.connect('mongodb+srv://alex:GabLou3145031820@cluster0.x11uct0.mongodb.net/twitIsMine?retryWrites=true&w=majority')
+exports.clientPromise = mongoose.connect(process.env.BDD)
 .then(()=>{console.log('connexion BDD réussi');})
 .catch(err=> console.log(err))
 
