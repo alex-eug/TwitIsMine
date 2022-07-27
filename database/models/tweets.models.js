04920499eab560aa600fa85ebce8c2d081c2a077
Alexandre Eugene <you@example.com>
2022-07-27 23:46:18 +0200

    envoi message

--- database/models/tweets.models.js
+++ database/models/tweets.models.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 const tweetSchema = schema({
-    content:{type:String,maxLength:140,minLength:1,required:true}
+    content: { type: String, maxLength: [140, 'Tweet trop long'], minLength: [2, 'Tweet trop court'], required: [true, 'champs requis'] }
 })
 
-const Tweet = mongoose.model('tweet',tweetSchema)
+const Tweet = mongoose.model('tweet', tweetSchema)
 
 module.exports = Tweet
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const api = require('./api')
 
+router.use('/api', api)
+
+router.get('/tweet/new', (req, res) => {
+  res.render('tweets/tweet-form');
+})
 router.get('/', (req, res) => {
-    res.render('tweets/tweet-list');
-  });
+  res.render('tweets/tweet-list');
+});
 
 module.exports = router
